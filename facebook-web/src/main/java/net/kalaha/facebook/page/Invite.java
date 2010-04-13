3dd91f8488354c1a64d55ddcd99ea591ec0ee743
Lars J. Nilsson <Lars J. Nilsson@localhost>
2010-04-13 22:59:18 +0200

    adding some missing files

---
+++ facebook-web/src/main/java/net/kalaha/facebook/page/Invite.java
@@ -0,0 +1,12 @@
+package net.kalaha.facebook.page;
+
+import org.apache.wicket.PageParameters;
+
+import net.kalaha.facebook.BasePage;
+
+public class Invite extends BasePage {
+
+	public Invite(PageParameters parameters) {
+		super(parameters);
+	}
+}
---
+++ persistence/src/test/java/net/kalaha/entities/GameTest.java
@@ -0,0 +1,24 @@
+package net.kalaha.entities;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GameTest {
+
+	@Test
+	public void getMyGame() {
+		User u1 = new User();
+		u1.setId(1);
+		
+		User u2 = new User();
+		u2.setId(2);
+		
+		Game g = new Game();
+		g.setOwner(u1);
+		g.setOpponent(u2);
+		
+		Assert.assertEquals(g.getMyOpponent(u1), u2);
+		Assert.assertEquals(g.getMyOpponent(u2), u1);
+		
+	}
+}
