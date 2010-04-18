925b321e64f99efdeaae85a71c184923b8e5e474
Lars J. Nilsson <Lars J. Nilsson@localhost>
2010-04-18 19:12:16 +0200

    adding firebase host/port to loading page

--- facebook-web/src/main/java/net/kalaha/facebook/page/Play.java
+++ facebook-web/src/main/java/net/kalaha/facebook/page/Play.java
@@ -4,12 +4,23 @@ import org.apache.log4j.Logger;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import com.restfb.FacebookException;
 
 import net.kalaha.facebook.BasePage;
 
 public class Play extends BasePage {
+	
+	@Inject
+	@Named("firebase-host")
+	private String firebaseHost;
 
+	@Inject
+	@Named("firebase-port")
+	private int firebasePort;
+
+	
 	public Play(PageParameters pars) {
 		super(pars);
 		try {
@@ -18,9 +29,10 @@ public class Play extends BasePage {
 			add(new Label("sessionId", getSession().getSession().getId()));
 			add(new Label("userId", String.valueOf(getSession().getUserId())));
 			add(new Label("gameId", pars.getString("gameId")));
+			add(new Label("firebaseHost", firebaseHost));
+			add(new Label("firebasePort", String.valueOf(firebasePort)));
 		} catch (FacebookException e) {
 			Logger.getLogger(getClass()).error("Facebook error", e);
 		}
-		
 	}
 }
